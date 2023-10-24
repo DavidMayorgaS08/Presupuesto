33c4c3410f39370f495f8db02ddb91afb91cc0d7
DavidMayorgaS08 <david08102@hotmail.com>
2023-10-23 22:32:42 -0500

    Inicios js

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-function registrar(){
-    let presupuesto = document.getElementById("presupuesto").value;
-    const numeroCompleto = presupuesto.toLocaleString('es', { minimumFractionDigits: 3, maximumFractionDigits: 3 });
-    console.log(numeroCompleto);
+function registrar() {
+    let gasto = document.getElementById("gasto").value;
+    let cant = document.getElementById("cant").value;
+
+    let informacion = [];
+    let gastos = {
+        gasto: gasto,
+        cant: cant,
+    }
+
+    informacion.push(gastos);
+
+    informacion.forEach((item, index) => {
+        let card_container = document.getElementById("cont_cards");
+        card_container.innerHTML = "";
+
+        let card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
+        <div class="art"><p>${item.gasto}</p></div>
+        <div class="precio"><p>${item.cant}</p></div>
+        <button class="boton_card">Eliminar</button>
+    `;
+        card_container.appendChild(card);
+    })
 }
